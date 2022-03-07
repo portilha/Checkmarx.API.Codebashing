97f53d86dcd11149ae76c4bc021dad63edd2133b
Pedro Portilha <pedro.portilha@checkmarx.com>
2022-03-07 15:49:55 -0800

    first mock version of the codebashing.

---
+++ Checkmarx.API.Codebashing.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Checkmarx.API.Codebashing.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
